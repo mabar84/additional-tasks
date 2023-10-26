9728b36d3242a1b91eb5863e17570323eff715cb
mabar84 <mabar@tut.by>
2023-10-26 23:57:10 +0300

    additional task with axios is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,87 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Tasks} from "./components/Tasks";
+import axios from 'axios';
 
-// Hi Guys!
-// Let's reinforce our current session!
-// -You have 2 arrays. You should create a new component TASKS, where you will render these arrays.
-// -Don't forget to assign types to our data.
+// Hi guys! Let`s reinforce our session:
+
+// 1. Install AXIOS -it`s a library for HTTP requests. We  use it instead method FETCH.
+// https://axios-http.com/ru/docs/intro
+// yarn add axios
+
+// axios.get('https://jsonplaceholder.typicode.com/todos')
+//     .then((res) => {
+//         setTodos(res.data)
+//     })
+
+//2. Let`s relocate our method map, and wrap it in a new variable:
+//const mapTodos=todos.map(el => {...
+
+// return (
+//     <div className="App">
+//         <button onClick={onClickHandler}>CLEAN POSTS</button>
+//         <ul>
+//             {mapTodos}
+//         </ul>
+//     </div>
+// );
+
+// 3. Create new button to redisplay  our data
+
+// 4. We are having a problem. The code is duplicated (axios.get...). Let`s create a new function and use it where we need.
+//Good luck!
+
+
+type PropsType =
+    {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean
+    }
 
 function App() {
-    const data1 = {
-        title: "What to do",
-        tasks: [
-            {taskId: 1, title: "HTML&CSS2", isDone: true},
-            {taskId: 2, title: "JS2", isDone: true}
-        ],
-        students: [
-            'Jago Wormald1',
-            'Saul Milne2',
-            'Aariz Hester3',
-            'Dion Reeve4',
-            'Anisa Ortega5',
-            'Blade Cisneros6',
-            'Malaikah Phelps7',
-            'Zeeshan Gallagher8',
-            'Isobella Vo9',
-            'Rizwan Mathis10',
-            'Menaal Leach11',
-            'Kian Walton12',
-            'Orion Lamb13',
-            'Faizah Huynh14',
-            'Crystal Vaughan15',
-            'Vivien Hickman16',
-            'Stuart Lu17',
-            'Karol Davison18',
-            'Dario Burns19',
-            'Chloe Rich20',
-            'Martyna Felix',
-            'Nida Glass',
-            'Maeve Miles',
-            'Hasnain Puckett',
-            'Ayman Cano',
-            'Safwan Perry',
-            'Fox Kelly',
-            'Louise Barlow',
-            'Malaki Mcgill',
-            'Leanna Cline',
-            'Willard Hodge',
-            'Amelia Dorsey',
-            'Kiah Porter',
-            'Jeanne Daly',
-            'Mohsin Armstrong',
-            'Laurie Rangel',
-            'Princess Tierney',
-            'Kasim Kendall',
-            'Darryl Cope',
-            'Elysha Ray',
-            'Liyana Harris',
-            'Kashif Blackburn',
-            'Atif Zimmerman',
-            'Sila Hartley',
-            'Ralphie Hebert',
-        ]
+    const [todos, setTodos] = useState<Array<PropsType>>([])
+
+    useEffect(() => {
+        // fetch('https://jsonplaceholder.typicode.com/todos')
+        //     .then(response => response.json())
+        //     .then(json => setTodos(json))
+        axiosRequest()
+
+    }, [])
+
+    const mapTodos = todos.map(el => {
+        return (
+            <li>
+                <span>{el.id} - </span>
+                <span>{el.title}</span>
+                <span>{el.completed}</span>
+            </li>
+        )
+    })
+
+    function axiosRequest() {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then((res) => {
+                setTodos(res.data)
+            })
     }
-    const data2 = {
-        title: "What to learn",
-        tasks: [
-            {taskId: 1, title: "HTML&CSS", isDone: true},
-            {taskId: 2, title: "JS", isDone: true}
-        ],
-        students: [
-            'Rick Kane',
-            'Finnlay Bentley',
-            'Samia North',
-            'Isaac Morton',
-            'Lily-Ann Clifford',
-            'Thalia Park',
-            'Sapphire Cruz',
-            'Cieran Vazquez',
-            'Anya Estes',
-            'Dominika Field',
-            'Rosanna Chung',
-            'Safiyah Davey',
-            'Ryley Beasley',
-            'Kalvin Trejo',
-            'Evie-Mae Farrell',
-            'Juliet Valencia',
-            'Astrid Austin',
-            'Lyle Montgomery',
-            'Nisha Mora',
-            'Kylie Callaghan',
-            'Star Wilks',
-            'Marissa Colley',
-            'Asa Fuller',
-            'Leigh Kemp',
-            'Avleen Dawson',
-            'Sammy Bonilla',
-            'Acacia Becker',
-            'Coral Shepherd',
-            'Melina Molina',
-            'Kiran Bailey',
-            'Clara Escobar',
-            'Alexandru Horn',
-            'Brandon-Lee Mercado',
-            'Elouise Weston',
-            'King Long',
-            'Kerri Searle',
-            'Kanye Hamer',
-            'Elwood Benitez',
-            'Mikail Whitaker',
-            'Bobby Hardy',
-            'Talha Ferry',
-            'Priscilla Landry',
-            'Olivia-Grace Cain',
-            'Kiaan Wallace',
-            'Wesley Padilla90',
-            'Ella-Grace Wooten91',
-            'Kaif Molloy92',
-            'Kamal Broadhurst93',
-            'Bianca Ferrell94',
-            'Micheal Talbot95',
-        ]
+
+    const onClickHandler = () => {
+        setTodos([])
+    }
+
+    const onClickRedisplayHandler = () => {
+        axiosRequest()
     }
 
     return (
         <div className="App">
-            <Tasks title={data1.title} tasks={data1.tasks} students={data1.students}/>
-            <Tasks title={data2.title} tasks={data2.tasks} students={data2.students}/>
+            <button onClick={onClickHandler}>CLEAN POSTS</button>
+            <button onClick={onClickRedisplayHandler}>REDISPLAY POSTS</button>
+            <ul>
+                {mapTodos}
+            </ul>
         </div>
     );
 }
@@ -133,180 +89,86 @@ function App() {
 export default App;
 
 
-//--------------------------------------------------------------
-// import React from 'react';
+//----------------------------------------------------------------------------------------
+
+// import React, {useEffect, useState} from 'react';
 // import './App.css';
-// import {Tasks} from "./Tasks";
+// import axios from "axios";
 //
-// export type DataType = {
-//     title: string
-//     tasks: Array<TasksType>
-//     students: Array<string>
-// }
-// export type TasksType = {
-//     taskId: number
-//     title: string
-//     isDone: boolean
-// }
 //
+// type PropsType =
+//     {
+//         userId: number,
+//         id: number,
+//         title: string,
+//         completed: boolean
+//     }
 //
 // function App() {
-//     const data1= {
-//         title: "What to do",
-//         tasks: [
-//             {taskId: 1, title: "HTML&CSS2", isDone: true},
-//             {taskId: 2, title: "JS2", isDone: true}
-//         ],
-//         students: [
-//             'Jago Wormald1',
-//             'Saul Milne2',
-//             'Aariz Hester3',
-//             'Dion Reeve4',
-//             'Anisa Ortega5',
-//             'Blade Cisneros6',
-//             'Malaikah Phelps7',
-//             'Zeeshan Gallagher8',
-//             'Isobella Vo9',
-//             'Rizwan Mathis10',
-//             'Menaal Leach11',
-//             'Kian Walton12',
-//             'Orion Lamb13',
-//             'Faizah Huynh14',
-//             'Crystal Vaughan15',
-//             'Vivien Hickman16',
-//             'Stuart Lu17',
-//             'Karol Davison18',
-//             'Dario Burns19',
-//             'Chloe Rich20',
-//             'Martyna Felix',
-//             'Nida Glass',
-//             'Maeve Miles',
-//             'Hasnain Puckett',
-//             'Ayman Cano',
-//             'Safwan Perry',
-//             'Fox Kelly',
-//             'Louise Barlow',
-//             'Malaki Mcgill',
-//             'Leanna Cline',
-//             'Willard Hodge',
-//             'Amelia Dorsey',
-//             'Kiah Porter',
-//             'Jeanne Daly',
-//             'Mohsin Armstrong',
-//             'Laurie Rangel',
-//             'Princess Tierney',
-//             'Kasim Kendall',
-//             'Darryl Cope',
-//             'Elysha Ray',
-//             'Liyana Harris',
-//             'Kashif Blackburn',
-//             'Atif Zimmerman',
-//             'Sila Hartley',
-//             'Ralphie Hebert',
-//         ]
+//     const [todos, setTodos] = useState<Array<PropsType>>([])
+//
+//     const axiosRequest=()=>{
+//         axios.get('https://jsonplaceholder.typicode.com/todos')
+//             .then((res) => {
+//                 setTodos(res.data)
+//             })
 //     }
-//     const data2 = {
-//         title: "What to learn",
-//         tasks: [
-//             {taskId: 1, title: "HTML&CSS", isDone: true},
-//             {taskId: 2, title: "JS", isDone: true}
-//         ],
-//         students: [
-//             'Rick Kane',
-//             'Finnlay Bentley',
-//             'Samia North',
-//             'Isaac Morton',
-//             'Lily-Ann Clifford',
-//             'Thalia Park',
-//             'Sapphire Cruz',
-//             'Cieran Vazquez',
-//             'Anya Estes',
-//             'Dominika Field',
-//             'Rosanna Chung',
-//             'Safiyah Davey',
-//             'Ryley Beasley',
-//             'Kalvin Trejo',
-//             'Evie-Mae Farrell',
-//             'Juliet Valencia',
-//             'Astrid Austin',
-//             'Lyle Montgomery',
-//             'Nisha Mora',
-//             'Kylie Callaghan',
-//             'Star Wilks',
-//             'Marissa Colley',
-//             'Asa Fuller',
-//             'Leigh Kemp',
-//             'Avleen Dawson',
-//             'Sammy Bonilla',
-//             'Acacia Becker',
-//             'Coral Shepherd',
-//             'Melina Molina',
-//             'Kiran Bailey',
-//             'Clara Escobar',
-//             'Alexandru Horn',
-//             'Brandon-Lee Mercado',
-//             'Elouise Weston',
-//             'King Long',
-//             'Kerri Searle',
-//             'Kanye Hamer',
-//             'Elwood Benitez',
-//             'Mikail Whitaker',
-//             'Bobby Hardy',
-//             'Talha Ferry',
-//             'Priscilla Landry',
-//             'Olivia-Grace Cain',
-//             'Kiaan Wallace',
-//             'Wesley Padilla90',
-//             'Ella-Grace Wooten91',
-//             'Kaif Molloy92',
-//             'Kamal Broadhurst93',
-//             'Bianca Ferrell94',
-//             'Micheal Talbot95',
-//         ]
+//
+//     useEffect(() => {
+//         // fetch('https://jsonplaceholder.typicode.com/todos')
+//         //     .then(response => response.json())
+//         //     .then(json => setTodos(json))
+//
+//         // axios.get('https://jsonplaceholder.typicode.com/todos')
+//         //     .then((res) => {
+//         //         setTodos(res.data)
+//         //     })
+//
+//         axiosRequest()
+//     }, [])
+//
+//     const mapTodos=todos.map(el=>{
+//         return (
+//             <li>
+//                 <span>{el.id} - </span>
+//                 <span>{el.title}</span>
+//                 <span>{el.completed}</span>
+//             </li>
+//         )
+//     })
+//
+//     const onClickHandler = () => {
+//         setTodos([])
+//     }
+//
+//     const onClickHandlerForRedisplay=()=>{
+//         // axios.get('https://jsonplaceholder.typicode.com/todos')
+//         //     .then((res) => {
+//         //         setTodos(res.data)
+//         //     })
+//
+//         axiosRequest()
 //     }
 //
 //     return (
 //         <div className="App">
-//             <Tasks data={data1}/>
-//             <Tasks data={data2}/>
+//             <button onClick={onClickHandler}>CLEAN POSTS</button>
+//             <button onClick={onClickHandlerForRedisplay}>REDISPLAY</button>
+//             <ul>
+//                 {/*{todos.map(el => {*/}
+//                 {/*    return (*/}
+//                 {/*        <li>*/}
+//                 {/*            <span>{el.id} - </span>*/}
+//                 {/*            <span>{el.title}</span>*/}
+//                 {/*            <span>{el.completed}</span>*/}
+//                 {/*        </li>*/}
+//                 {/*    )*/}
+//                 {/*})}*/}
+//
+//                 {mapTodos}
+//             </ul>
 //         </div>
 //     );
 // }
 //
 // export default App;
-
-// import React from 'react';
-// import {DataType} from "./App";
-//
-// type  TasksPropsType = {
-//     data: DataType
-// }
-//
-// export const Tasks = (props: TasksPropsType) => {
-//     return (
-//         <div>
-//             <h1>{props.data.title}</h1>
-//             <ul>
-//                 {props.data.tasks.map(el => {
-//                     return (
-//                         <li>
-//                             <span>{el.taskId}</span>
-//                             <span>{el.title}</span>
-//                             <span>{el.isDone}</span>
-//                         </li>
-//                     )
-//                 })}
-//             </ul>
-//
-//             <ul>
-//                 {props.data.students.map(el => {
-//                     return (
-//                         <li>{el}</li>
-//                     )
-//                 })}
-//             </ul>
-//         </div>
-//     );
-// };
-
-//--------------------------------------------------------------
